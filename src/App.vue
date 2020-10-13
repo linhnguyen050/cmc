2bc49309f4a06f03d7307bd983008945a5b1d020
VG (Village Ghost) <hai.nguyen@twentyci.asia>
2020-10-13 16:42:08 +0700

    xoa Helloword

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DangNhap/>
   </div>
 </template>
 
 <script>
-
-import HelloWorld from './components/HelloWorld.vue'
-
-
+import DangNhap from './components/DangNhap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DangNhap
   }
 }
 </script>
